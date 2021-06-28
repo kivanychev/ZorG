e0b9ba9a4da49408272cca39db84efd383d66608
kivanych <kirill.ivanychev@zorgvision.ru>
2021-06-28 12:47:21 +0300

    Initial commit

---
+++ Src/DiscoTest/Core/Inc/main.h
@@ -0,0 +1,120 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "stm32746g_discovery.h"
+#include "stm32746g_discovery_ts.h"
+#include "stm32746g_discovery_lcd.h"
+#include "stm32746g_discovery_sdram.h"
+#include "stm32746g_discovery_camera.h"
+#include "stm32746g_discovery_audio.h"
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+
+#define RGB565_BYTE_PER_PIXEL     2
+#define ARBG8888_BYTE_PER_PIXEL   4
+
+/* Camera have a max resolution of VGA : 640x480 */
+#define CAMERA_RES_MAX_X          640
+#define CAMERA_RES_MAX_Y          480
+
+/**
+  * @brief  LCD FB_StartAddress
+  * LCD Frame buffer start address : starts at beginning of SDRAM
+  */
+#define LCD_FRAME_BUFFER          SDRAM_DEVICE_ADDR
+
+/**
+  * @brief  Camera frame buffer start address
+  * Assuming LCD frame buffer is of size 480x800 and format ARGB8888 (32 bits per pixel).
+  */
+#define CAMERA_FRAME_BUFFER       ((uint32_t)(LCD_FRAME_BUFFER + (RK043FN48H_WIDTH * RK043FN48H_HEIGHT * ARBG8888_BYTE_PER_PIXEL)))
+
+/**
+  * @brief  SDRAM Write read buffer start address after CAM Frame buffer
+  * Assuming Camera frame buffer is of size 640x480 and format RGB565 (16 bits per pixel).
+  */
+#define SDRAM_WRITE_READ_ADDR        ((uint32_t)(CAMERA_FRAME_BUFFER + (CAMERA_RES_MAX_X * CAMERA_RES_MAX_Y * RGB565_BYTE_PER_PIXEL)))
+
+#define SDRAM_WRITE_READ_ADDR_OFFSET ((uint32_t)0x0800)
+#define SRAM_WRITE_READ_ADDR_OFFSET  SDRAM_WRITE_READ_ADDR_OFFSET
+
+#define AUDIO_REC_START_ADDR         SDRAM_WRITE_READ_ADDR
+
+/* The Audio file is flashed with ST-Link Utility @ flash address =  AUDIO_SRC_FILE_ADDRESS */
+#define AUDIO_SRC_FILE_ADDRESS       0x08080000   /* Audio file address in flash */
+
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+void SDRAM_demo(void);
+void Camera_demo(void);
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(uint8_t* file, uint32_t line);
+#endif
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
